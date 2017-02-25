4f43130a5a86f4a012e00551cb4c535851f8a60c
Nikas <nikas.lebedenko@gmail.com>
2017-02-25 21:17:23 +0200

    UsersRequest + Create Users Page - Users Photo

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Users</h1>
+
+    @if($users)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Is active</th>
+                    <th>Created</th>
+                    <th>Updated</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td>{{($user->is_active == 1) ? 'active' : 'disabled'}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @endif
+@stop
